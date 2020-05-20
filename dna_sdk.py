347d9c32eb787fbd74937e77f801e562b1d1d5dc
Fodor <fodorj@c02yl5qklvcg.wwt.com>
2020-05-19 20:43:15 -0500

    updating dna_sdk to make more sense and better example.

--- dna_sdk.py
+++ dna_sdk.py
@@ -4,26 +4,25 @@ from configparser import ConfigParser
 
 config = ConfigParser() # using a .ini file to hide my personal home meraki api key :)
 ini = config.read('config.ini') # reading ini file
-username = str(config.get('config', 'username')) # openting the .ini file to get the api key 
+username = str(config.get('config', 'username')) # opening the .ini file to get the api key 
 password = str(config.get('config', 'password'))
 base_url = str(config.get('config', 'base_url'))
 def main():
     try:
-        api = DNACenterAPI(username=username, password=password, base_url=base_url, verify=False)
+        api = DNACenterAPI(username=username, password=password, base_url=base_url, verify=False) # This handles gather the token 
 
-        get_discovery = api.clients.get_overall_client_health()
-        get_site = api.sites.get_site_count()
-        print (get_site)
+        get_discovery = api.clients.get_overall_client_health() # Getting overall client health
+        client_count = get_discovery['response'][0]['scoreDetail'][0]['clientUniqueCount'] # parsing down to get global client count
+    
+        print (f"\nthe global client count is: {client_count}") # printing client count
 
-        get_device_count = api.devices.get_device_config_for_all_devices()
-        get_device = api.devices.retrieve_all_network_devices()
-        print(get_device_count)
-        get_device_list = api.devices.get_device_list()
-        pprint (get_device_list)
+        get_site = api.sites.get_site_count() # getting number of sites created
+        print (f"The number of sites are: {get_site['response']}") # printing number of sites.
+        
     except ApiError as e:
         print(e)
-get_device_list
+
 if __name__ == "__main__":
     main()
 
-# use help(api.devices)
+# use help(api.devices) or help(api.sites) to get all the possible methods. Also help(api.pnp)
